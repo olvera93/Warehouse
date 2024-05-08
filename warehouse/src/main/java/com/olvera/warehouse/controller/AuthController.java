b7b07857f10d31f9f4862cd754a49bf70789292f
olvera93 <olvera9302@outlook.com>
2024-05-08 16:28:28 -0600

    [FIX] I fixed it all the backend project with spring security and jwt

---
+++ warehouse/src/main/java/com/olvera/warehouse/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.olvera.warehouse.controller;
+
+import com.olvera.warehouse.model.AuthResponse;
+import com.olvera.warehouse.model.LoginRequest;
+import com.olvera.warehouse.model.RegisterRequest;
+import com.olvera.warehouse.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
+    {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
+    {
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
