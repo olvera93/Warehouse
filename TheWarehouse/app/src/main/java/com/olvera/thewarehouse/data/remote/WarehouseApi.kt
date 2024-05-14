f4d5a634c53bd8da4bf8893e4c86bcb799f8f4df
olvera93 <olvera9302@outlook.com>
2024-05-14 12:24:36 -0600

    [ADD] logo animator

---
+++ TheWarehouse/app/src/main/java/com/olvera/thewarehouse/data/remote/WarehouseApi.kt
@@ -0,0 +1,13 @@
+package com.olvera.thewarehouse.data.remote
+
+import com.olvera.thewarehouse.model.User
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface WarehouseApi {
+
+    @POST
+    suspend fun signUp(@Body user: User): Response<User>
+
+}
