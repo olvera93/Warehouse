eea10e9ddf1be3adbee8de355debcc3f7089b950
olvera93 <olvera9302@outlook.com>
2024-06-06 14:36:03 -0600

    [ADD] Theme

--- TheWarehouse/app/src/main/java/com/olvera/thewarehouse/components/ProductCard.kt
+++ TheWarehouse/app/src/main/java/com/olvera/thewarehouse/components/ProductCard.kt
@@ -1,14 +1,23 @@
 package com.olvera.thewarehouse.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 @Composable
@@ -19,21 +28,38 @@ fun CategoryCard(
     onClick: () -> Unit
 ) {
 
-    Box(modifier = Modifier){
+    Box(
+        modifier = Modifier.padding(8.dp),
 
-        Column {
+
+    ){
+        Card(
+            modifier = Modifier
+                .size(width = 200.dp, height = 330.dp)
+                .clickable { onClick() },
+            shape = RoundedCornerShape(10.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )
+        ) {
             AsyncImage(
                 model = image,
                 contentDescription = title,
-                modifier = Modifier.height(250.dp),
-                contentScale = ContentScale.Crop
+                modifier = Modifier
+                    .height(250.dp)
+                    .width(200.dp),
+                contentScale = ContentScale.Fit,
             )
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = if (title.length > 15) title.substring(0, 15) + "..." else title, color = Color.Black,
+                fontSize = 14.sp,
+                maxLines = 1,
 
-            Text(text = title, color= Color.White)
-
+            )
         }
-
-
     }
-
 }
--- TheWarehouse/app/src/main/java/com/olvera/thewarehouse/presentation/product/ProductView.kt
+++ TheWarehouse/app/src/main/java/com/olvera/thewarehouse/presentation/product/ProductView.kt
@@ -2,9 +2,9 @@ package com.olvera.thewarehouse.presentation.product
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,17 +12,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import com.google.accompanist.pager.rememberPagerState
 import com.google.accompanist.pager.HorizontalPager
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.olvera.thewarehouse.components.CategoryCard
 
@@ -43,13 +46,16 @@ fun ProductView(
 
     Column(
         modifier = Modifier
+            .padding(8.dp)
             .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         HorizontalPager(
-            modifier = Modifier.height(250.dp),
+            modifier = Modifier.height(300.dp).fillMaxSize(),
             state = pagerState,
-            itemSpacing = 16.dp
+            itemSpacing = 16.dp,
+            horizontalAlignment = Alignment.Start
+
         ) { product ->
             products[product].images.firstOrNull()?.let { image ->
                 products[product].id?.let { id ->
@@ -73,7 +79,10 @@ fun ProductView(
                         .padding(16.dp),
                     style = MaterialTheme.typography.bodyLarge,
                     text = category.name,
-                    color = MaterialTheme.colorScheme.onSurface
+                    color = Color.Black,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
                 )
 
                 val productsInCategory = products.filter { it.category == category.slug }
