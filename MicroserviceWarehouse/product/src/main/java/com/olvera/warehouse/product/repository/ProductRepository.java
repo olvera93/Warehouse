add2cb686ce2db0c0c839a29840c3e08bd8ed018
olvera93 <olvera9302@outlook.com>
2024-08-19 13:45:53 -0600

    [ADD] Pageable and I created new endpoint "getUsersProducts"

--- MicroserviceWarehouse/product/src/main/java/com/olvera/warehouse/product/repository/ProductRepository.java
+++ MicroserviceWarehouse/product/src/main/java/com/olvera/warehouse/product/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.olvera.warehouse.product.repository;
 
 import com.olvera.warehouse.product.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+
+    Page<Product> findByUserId(Integer userId, Pageable pageable);
 }
