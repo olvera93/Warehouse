b60acd0a372037e672258d60e145cadd359d40cc
olvera93 <olvera9302@outlook.com>
2024-08-27 12:22:13 -0600

    [ADD] Unit tests cases for Warehouse project

---
+++ MicroserviceWarehouse/warehouse/src/main/java/com/olvera/warehouse/dto/CartItemResponseClient.java
@@ -0,0 +1,32 @@
+package com.olvera.warehouse.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemResponseClient {
+
+    private String productId;
+
+    @Schema(name = "productName", description = "The product's name", example = "Pizza")
+    private String productName;
+
+    @Schema(name = "price", description = "Price of the product", example = "15.20")
+    private double total;
+
+    @Schema(name = "quantity", description = "Quantity of the product", example = "3")
+    private  int quantity;
+
+    @Schema(name = "image", description = "Image of the product", example = "Pizza")
+    private String image;
+
+    @Schema(name = "userId", description = "To which product(s) the user belongs ", example = "2")
+    Integer userId;
+
+}
