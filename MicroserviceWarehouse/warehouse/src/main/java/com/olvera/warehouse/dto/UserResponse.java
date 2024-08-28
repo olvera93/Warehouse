4f1a85caecd232577b0b39f151747ae06ad68124
olvera93 <olvera9302@outlook.com>
2024-08-28 16:19:23 -0600

    [ADD] Caching for warehouse

--- MicroserviceWarehouse/warehouse/src/main/java/com/olvera/warehouse/dto/UserResponse.java
+++ MicroserviceWarehouse/warehouse/src/main/java/com/olvera/warehouse/dto/UserResponse.java
@@ -11,11 +11,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserResponse {
+public class UserResponse implements Serializable {
 
     Integer userId;
 
