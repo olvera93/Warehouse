c971f670be5f93eb824ef753f1d022f6f2154846
olvera93 <olvera9302@outlook.com>
2024-08-28 13:18:44 -0600

    [ADD] Caching

--- MicroserviceWarehouse/product/src/main/java/com/olvera/warehouse/product/util/AppConstants.java
+++ MicroserviceWarehouse/product/src/main/java/com/olvera/warehouse/product/util/AppConstants.java
@@ -4,4 +4,8 @@ public class AppConstants {
 
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "5";
+
+    // Cache
+    public static final String PRODUCT_CACHE_LIST = "products";
+    public static final String PRODUCT_CACHE = "product";
 }
